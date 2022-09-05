2397345943edde8ccf4cad449ec670d504681c39
dior <diorrego1@gmail.com>
2022-09-05 06:10:45 -0400

    :construction: implement notion stow docs

--- components/NotionPage/NotionPage.tsx
+++ components/NotionPage/NotionPage.tsx
@@ -1,15 +1,19 @@
+import React from 'react';
 import Head from 'next/head'
+import Image from 'next/image';
+import Link from 'next/link';
 
 import { NotionRenderer } from 'react-notion-x'
 import { ExtendedRecordMap } from 'notion-types'
 import { getPageTitle } from 'notion-utils'
+import {defaultPageCoverPosition} from '../../lib/config'
 
 const NotionPage = ({
   recordMap,
-  rootPageId
+  rootPageId,
 } : {
   recordMap: ExtendedRecordMap
-  rootPageId?: string
+  rootPageId?: string,
 }) => {
   if (!recordMap) {
     return null
@@ -24,9 +28,15 @@ const NotionPage = ({
       </Head>
       <NotionRenderer
         recordMap={recordMap}
+        previewImages={!!recordMap.preview_images}
         fullPage={true}
         darkMode={false}
         rootPageId={rootPageId}
+        components={{
+          nextImage: Image,
+          nextLink: Link,
+        }}
+        defaultPageCoverPosition={defaultPageCoverPosition}
       />
     </>
   )
